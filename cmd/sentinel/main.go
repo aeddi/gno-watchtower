b2a6b213fad05cd606edbdb7b373df0c066a243e
aeddi <antoine.e.b@gmail.com>
2026-04-01 10:19:51 +0200

    feat: add --log-format and --log-level flags to sentinel run subcommand

--- cmd/sentinel/main.go
+++ cmd/sentinel/main.go
@@ -3,7 +3,9 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"log"
 	"log/slog"
 	"os"
 	"os/signal"
@@ -11,7 +13,7 @@ import (
 
 	"github.com/gnolang/val-companion/internal/sentinel/app"
 	"github.com/gnolang/val-companion/internal/sentinel/config"
-	"github.com/gnolang/val-companion/pkg/logger"
+	pkglogger "github.com/gnolang/val-companion/pkg/logger"
 )
 
 func main() {
@@ -21,7 +23,7 @@ func main() {
 	}
 	switch os.Args[1] {
 	case "run":
-		runCmd()
+		runCmd(os.Args[2:])
 	case "generate-config":
 		fmt.Print(config.Example)
 	case "doctor":
@@ -33,29 +35,57 @@ func main() {
 	}
 }
 
-func runCmd() {
-	if len(os.Args) < 3 {
-		fmt.Fprintln(os.Stderr, "Usage: sentinel run <config-file>")
+func runCmd(args []string) {
+	fs := flag.NewFlagSet("run", flag.ExitOnError)
+	logFormat := fs.String("log-format", "console", "log output format: console, json, journal")
+	logLevel := fs.String("log-level", "info", "minimum log level: debug, info, warn, error")
+	fs.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: sentinel run [--log-format=...] [--log-level=...] <config-file>")
+		fs.PrintDefaults()
+	}
+	if err := fs.Parse(args); err != nil {
 		os.Exit(1)
 	}
-	cfg, err := config.Load(os.Args[2])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "load config: %v\n", err)
+	if fs.NArg() < 1 {
+		fs.Usage()
 		os.Exit(1)
 	}
 
-	log, err := logger.New(logger.FormatConsole, slog.LevelInfo)
+	cfg, err := config.Load(fs.Arg(0))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "init logger: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("load config: %v", err)
+	}
+
+	level, err := parseLevel(*logLevel)
+	if err != nil {
+		log.Fatalf("invalid log level: %v", err)
+	}
+	logger, err := pkglogger.New(pkglogger.Format(*logFormat), level)
+	if err != nil {
+		log.Fatalf("init logger: %v", err)
 	}
 
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	app.Run(ctx, cfg, log)
+	app.Run(ctx, cfg, logger)
+}
+
+func parseLevel(s string) (slog.Level, error) {
+	switch s {
+	case "debug":
+		return slog.LevelDebug, nil
+	case "info":
+		return slog.LevelInfo, nil
+	case "warn":
+		return slog.LevelWarn, nil
+	case "error":
+		return slog.LevelError, nil
+	default:
+		return slog.LevelInfo, fmt.Errorf("unknown level %q: must be debug, info, warn, or error", s)
+	}
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: sentinel <command> [args]\n\nCommands:\n  run <config>     Start the sentinel\n  generate-config  Print example config to stdout\n  doctor <config>  Check config and setup\n")
+	fmt.Fprintf(os.Stderr, "Usage: sentinel <command> [args]\n\nCommands:\n  run [--log-format=...] [--log-level=...] <config>  Start the sentinel\n  generate-config                                    Print example config to stdout\n  doctor <config>                                    Check config and setup\n")
 }
